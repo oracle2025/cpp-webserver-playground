fa04bb93ef97acd65277a6d3c30a8eedf2752a4f
Richard Spindler <richard.spindler@gmail.com>
2023-12-14 23:16:05 +0100

    fixing tests

--- tests/InputTest.cpp
+++ tests/InputTest.cpp
@@ -7,17 +7,17 @@ TEST_CASE("Text Element")
 {
     CHECK(
         Input::Text("username")()
-        == R"(<label for="username">Username</label><br> <input type="text" id="username" name="username" value="">)");
+        == R"(<label for="username">Username</label><br> <input class="form-control" type="text" id="username" name="username" value="">)");
 }
 
 TEST_CASE("Password Element")
 {
     CHECK(
         Input::Password("password")()
-        == R"(<label for="password">Password</label><br> <input type="password" id="password" name="password" value="">)");
+        == R"(<label for="password">Password</label><br> <input class="form-control" type="password" id="password" name="password" value="">)");
     CHECK(
         Input::Password("password2")()
-        == R"(<label for="password2">Password2</label><br> <input type="password" id="password2" name="password2" value="">)");
+        == R"(<label for="password2">Password2</label><br> <input class="form-control" type="password" id="password2" name="password2" value="">)");
 }
 
 TEST_CASE("Form Data")
