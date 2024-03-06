c34f785dc7211786b54a1da484d97f95f400a9c9
Richard Spindler <richard.spindler@gmail.com>
2024-03-06 17:32:45 +0100

    Removing Code:
     - Read from File

--- src/FileSystem/ReadFromFile.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-namespace FileSystem {
-class ReadFromFile {
-public:
-    static std::string read_string_from_file(const std::string& filename);
-};
-} // namespace FileSystem
