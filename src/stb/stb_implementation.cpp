efe3247278476fcb2ceeeae8b2741f756f03fda3
Richard Spindler <richard.spindler@gmail.com>
2024-03-06 16:38:54 +0100

    Removing Code:
     - Image Browser

--- src/stb/stb_implementation.cpp
+++
@@ -1,6 +0,0 @@
-#define STB_IMAGE_IMPLEMENTATION
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include "stb_image_write.h"
-#include "stb_image_resize.h"
-#include "stb_image.h"
