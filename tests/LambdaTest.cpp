dc770d13bd09ed110b0e16cc9021c0e67b064b13
Richard Spindler <richard.spindler@gmail.com>
2024-04-26 17:06:26 +0200

    lambda delete test case

---
+++ tests/LambdaTest.cpp
@@ -0,0 +1,40 @@
+#include "doctest.h"
+
+struct ReferenceCounting {
+    ReferenceCounting()
+    {
+        references++;
+    }
+    ~ReferenceCounting()
+    {
+        references--;
+    }
+    static int references;
+};
+int ReferenceCounting::references = 0;
+
+TEST_CASE("Test Custom Reference Counter")
+{
+    CHECK_EQ(ReferenceCounting::references, 0);
+    {
+        ReferenceCounting first;
+        CHECK_EQ(ReferenceCounting::references, 1);
+        ReferenceCounting second;
+        CHECK_EQ(ReferenceCounting::references, 2);
+    }
+    CHECK_EQ(ReferenceCounting::references, 0);
+}
+
+TEST_CASE("Check if Lambda Deletes Shared Ptr")
+{
+    CHECK_EQ(ReferenceCounting::references, 0);
+    std::function<int()> myFunction;
+    {
+        auto first = std::make_shared<ReferenceCounting>();
+        CHECK_EQ(ReferenceCounting::references, 1);
+        myFunction = [first]() { return 12; };
+    }
+    CHECK_EQ(ReferenceCounting::references, 1);
+    myFunction = [](){return 13;};
+    CHECK_EQ(ReferenceCounting::references, 0);
+}
