5f685873d6b278b3f1598db72b33a36453ef3cf7
Richard Spindler <richard.spindler@gmail.com>
2024-07-15 22:48:50 +0200

    add userlist, signup to release

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 mkdir -p build-release
 cd build-release
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release ..
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DENABLE_USER_LIST=On -DENABLE_SIGNUP=On ..
 ninja
 cpack
 dpkg -c ./*.deb
